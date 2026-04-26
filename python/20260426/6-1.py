2dfddb390c81847fc939d21cb826dcbf7c5afe8b
InkSeason <huangequel@qq.com>
2026-04-26 23:48:29 +0800

    20260426

---
+++ python/20260426/6-1.py
@@ -0,0 +1,6 @@
+profiles = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York',
+}
+print(profiles)
---
+++ python/20260426/6-2.py
@@ -0,0 +1,6 @@
+favorite_numbers = {
+    'Alice': [3, 7, 9],
+    'Bob': [2, 4, 6],
+    'Charlie': [1, 5, 8],
+}
+print(favorite_numbers)
